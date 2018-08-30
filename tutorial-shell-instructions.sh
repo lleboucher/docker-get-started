f4d717386e27d8103b6997212935c08c79bb2188
Thierry Souche <thierry.souche@gmail.com>
2018-08-30 15:58:52 +0200

    Enriched the shell instructions file with Part 3 on 'services'

--- tutorial-shell-instructions.sh
+++ tutorial-shell-instructions.sh
@@ -26,8 +26,9 @@ docker run -p 4000:80 friendlyhello
 docker run -d -p 4000:80 friendlyhello
 
 # To test the service, use a browser:
-# The Python app is serving the http://0.0.0.0:80 (as seen from the inside of the container) but the docker instruction
-# maps the port 80 to the external port 4000, go to the URL http://localhost:4000
+# The Python app is serving the http://0.0.0.0:80 (as seen from the inside of the
+# container) but the docker instruction maps the port 80 to the external port 4000,
+# go to the URL http://localhost:4000
 
 # see the containers which were created
 
@@ -37,3 +38,36 @@ docker container ls
 # CONTAINER ID        IMAGE               COMMAND             CREATED
 # 1fa4ab2cf395        friendlyhello       "python app.py"     28 seconds ago
 
+
+# Share the image on a public repository
+# Log into the repository
+docker login
+
+# Tag the image
+
+# The notation for associating a local image with a repository on a registry 
+# is username/repository:tag. The tag is optional, but recommended, since it
+# is the mechanism that registries use to give Docker images a version. Give
+# the repository and tag meaningful names for the context, such as 
+# get-started:part2. This puts the image in the get-started repository and 
+# tag it as part2.
+
+# Now, put it all together to tag the image. Run docker tag image with your
+# username, repository, and tag names so that the image uploads to your
+# desired destination. The syntax of the command is:
+#
+# docker tag image username/repository:tag
+# 
+# For example:
+
+docker tag friendlyhello tsouche/get-started:part2
+
+# Publish the image:
+# Upload your tagged image to the repository:
+
+docker push username/repository:tag
+
+# Pull and run the image from the remote repository
+# From now on, you can use docker run and run your app on any machine with this command:
+
+docker run -p 4000:80 username/repository:tag
